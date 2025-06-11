306701d7253ab4092fac226abc9be103bb83fffa
DevJoece <7666.2023@students.ku.ac.ke>
2025-06-11 21:28:28 +0300

    first commit

---
+++ OTP/main.js
@@ -0,0 +1,44 @@
+const inputs = document.querySelectorAll("input")
+let btnVerify = document.querySelector("#verify-btn")
+
+inputs.forEach((input,index1) => {
+    input.addEventListener("keyup",(e) =>{
+        const currentInput = input,
+           nextInput = input.nextElementSibling,
+           prevInput = input.previousElementSibling
+        // if the value has more than one character clear it
+           if(currentInput.value.length > 1){
+             currentInput.value = ""
+             return 
+           }
+
+           // if the next input is diasbled and current value is not empty
+           // enable the next input and focus on it
+           if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== ""){
+               nextInput.removeAttribute("disabled")
+               nextInput.focus()
+           }
+
+           // if backspace key is pressed
+           if(e.key === "Backspace"){
+               //iterate over all input
+               inputs.forEach((input, index2) => {
+                  if(index1 <= index2 && prevInput){
+                      input.setAttribute("disabled",true)
+                      currentInput.value = ""
+                  }
+               })
+           }
+           if(!inputs[5].disabled && inputs[5].value !== ""){
+               btnVerify.classList.add("active")
+               return;
+           }
+           btnVerify.classList.remove("active")
+    })
+})  
+        
+
+
+    
+
+window.addEventListener("load",() => inputs[0].focus())
---
+++ reset password/main.js
@@ -0,0 +1,56 @@
+let inputPassword = document.getElementById('password')
+let inputConfirmPassword = document.getElementById('confirm-password')
+let message = document.querySelector('.message')
+let eyePassword = document.getElementById('eye-password')
+let eyeConfirm = document.getElementById('eye-confirm')
+let form = document.getElementById('form')
+
+form.addEventListener('submit',confirm)
+eyePassword.addEventListener('click',togglePassword)
+eyeConfirm.addEventListener('click',toggleConfirmPassword)
+
+
+function confirm(e){
+    e.preventDefault()
+    console.log(inputPassword.value,inputConfirmPassword.value)
+    if(inputPassword.value !== 0){
+        if(inputPassword.value === inputConfirmPassword.value){
+            message.innerHTML = ''
+            inputConfirmPassword.style.borderColor =' #BFBFBF'
+         }else{
+            message.innerHTML='password does not match'
+            inputConfirmPassword.style.borderColor =' #EB4335'
+         }
+    }
+    if(inputPassword.value === ''){
+        alert('please enter your password')
+    }
+
+
+}
+
+function togglePassword(e){
+    if(inputPassword.type == "password"){
+        inputPassword.type = "text"
+        eyePassword.classList.remove("fa-regular fa-eye-slash")
+        eyePassword.classList.add("fa-regular fa-eye")
+    }else{
+        inputPassword.type = "password"
+        eyePassword.classList.remove("fa-regular fa-eye")
+        eyePassword.classList.add("fa-regular fa-eye-slash")
+    }
+
+
+}
+
+function toggleConfirmPassword(e){
+        if(inputConfirmPassword.type == "password"){
+            inputConfirmPassword.type = "text"
+            eyeConfirm.classList.remove("fa-regular fa-eye-slash")
+            eyeConfirm.classList.add("fa-regular fa-eye")
+        }else{
+            inputConfirmPassword.type = "password"
+            eyeConfirm.classList.remove("fa-regular fa-eye")
+            eyeConfirm.classList.add("fa-regular fa-eye-slash")
+        }
+    }
